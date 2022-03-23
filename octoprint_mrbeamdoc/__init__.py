990954f786d24deeddc2feba2c03dac34c36cffd
Josef-MrBeam <josef@mr-beam.org>
2022-03-23 09:56:15 +0100

    update versioneer to 0.18

--- octoprint_mrbeamdoc/__init__.py
+++ octoprint_mrbeamdoc/__init__.py
@@ -2,10 +2,13 @@
 # coding=utf-8
 import os
 
-from __version import __version__
-
 DOC_ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 def get_version():
     return __version__
+
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
